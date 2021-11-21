f3812ffe4fa54f32774ad0704f296ef2361cac26
darthcerellius <71949550+darthcerellius@users.noreply.github.com>
2021-11-21 14:15:03 +0200

    Development states (#6)
    * Added states file to project
    
    * Created main function which utilises the states and code_loader code.
    
    * Fixed failing unit test
    
    * Added states
    
    * Added dependency to make static array for holding state transition functions
    
    * Added dependency to make static array for holding state transition functions
    
    * Modified state to return the result of the execution.
    
    Co-authored-by: Kyle Barry <kylebarry05@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+#[macro_use]
+extern crate lazy_static;
+
 mod code_loader;
 mod errors;
 mod states;
 
 use std::process::exit;
-use crate::states::{ExecuteState, StateMachine};
+use crate::states::{States, get_state};
 
 fn main() {
     let program_file = std::env::args().nth(1);
@@ -12,15 +15,16 @@ fn main() {
             let program = code_loader::load_code_from_file(data);
             match program {
                 Ok((mut reg_data, code_data)) => {
-                    let mut state = ExecuteState{}.execute(&mut reg_data, &code_data, 0);
+                    let mut state = get_state(States::ExecuteState).execute(&mut reg_data, &code_data, 0);
                     loop {
-                        let state_function = state.1;
-                        match state_function {
-                            Some(ref new_state) => state = new_state.execute(&mut reg_data, &code_data, state.0),
-                            None => exit(0)
+                        if state.as_ref().is_err() {
+                            eprintln!("{}", state.err().unwrap());
+                            exit(-1);
                         }
+                        let state_function = &state.as_ref().ok().unwrap().1;
+                        let new_state = state.as_ref().ok().unwrap().0;
+                        state_function.execute(&mut reg_data, &code_data, new_state);
                     }
-
                 },
                 Err(error_msg) => {
                     eprintln!("{}", error_msg);
